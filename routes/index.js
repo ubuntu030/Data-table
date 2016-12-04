fe613530964476441f47eb10486f45841f139915
ubuntu030 <Ubuntu030@gmail.com>
2016-12-04 21:44:59 +0800

    add demoV1 demoV2 router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: 'Express'});
 });
+
+router.get('/v1', function(req, res) {
+    res.render('demoV1', {
+        title: 'Use jQuery',
+        descript: 'This is my first example'
+    })
+})
+
+router.get('/v2', function(req, res) {
+    res.render('demoV2', {
+        title: 'Use jQuery',
+        descript: 'Optimizing Javascript code'
+    })
+})
 
 module.exports = router;
